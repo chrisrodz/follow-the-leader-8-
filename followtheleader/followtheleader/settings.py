e8de859f188c9e266376246d59196a3434df8678
chrisrodz <christian.etpr10@gmail.com>
2013-04-20 19:24:54 -0400

    Heroku db is setup. Yamil did all the real work for this

--- followtheleader/followtheleader/settings.py
+++ followtheleader/followtheleader/settings.py
@@ -13,13 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ccomforms',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd587p367n89slk',
+        'USER': 'ekgembattbygkh',
+        'PASSWORD': '2liAtE0WE7ceNEVblUTahMPUX2',
+        'HOST': 'ec2-54-225-105-169.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
--- followtheleader/followtheleader/urls.py
+++ followtheleader/followtheleader/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+	url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
