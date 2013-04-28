7da1b04cf6face5b557dd49c1bcaa56ef3655ac2
chrisrodz <christian.etpr10@gmail.com>
2013-04-28 18:03:13 -0400

    Added gunicorn for heroku deployment

--- followtheleader/ccomforms/admin.py
+++ followtheleader/ccomforms/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from ccomforms.models import T02, A125, T02Form
+# from django.contrib import admin
+# from ccomforms.models import T02, A125, T02Form
 
-class T02Admin(admin.ModelAdmin):
-	form = T02Form
-	readonly_fields = T02._meta.get_all_field_names()
+# class T02Admin(admin.ModelAdmin):
+# 	form = T02Form
+# 	readonly_fields = T02._meta.get_all_field_names()
 
-admin.site.register(T02, T02Admin)
-admin.site.register(A125)
+# admin.site.register(T02, T02Admin)
+# admin.site.register(A125)
